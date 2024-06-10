4158f4a968f24db192cf9645b2db4faa8bd56c4d
Benji Loya <benjiloya.apple@gmail.com>
2024-06-11 00:47:36 +0500

    Initial Commit

---
+++ MultiMediaPicker/MultiMediaPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMediaPickerApp.swift
+//  MultiMediaPicker
+//
+//  Created by Benji Loya on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiMediaPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
